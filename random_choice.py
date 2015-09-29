7e52bc29de62730a9815925034c01e02aaea58bf
cmaher92 <connor.maher@students.makeschool.com>
2015-09-29 12:36:26 -0700

    Completed the simple linear approach to weighting values in a container.

--- random_choice.py
+++ random_choice.py
@@ -1,7 +1,18 @@
 import random
-import sys
 
 
-choices = ['unknown']*70 + ['yes']*15 + ['no']*15
-random_choice = random.shuffle(choices)
-print(random_choice)
+def weighted_choice(weights):
+    totals = []
+    running_total = 0
+
+    for w in weights:
+        running_total += w
+        totals.append(running_total)
+
+    rnd = random.random() * running_total
+    for i, total in enumerate(totals):
+        if rnd < total:
+            print(i)
+if __name__ == '__main__':
+    weights = [2, 3, 5]
+    weighted_choice(weights)
