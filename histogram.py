e741b8fefa8a72b986bcd243b265de257088c2de
cmaher92 <connor.maher@students.makeschool.com>
2015-10-02 13:13:27 -0700

    Added find method, list method, and another histogram

--- histogram.py
+++ histogram.py
@@ -19,6 +19,26 @@ def histogram(word_list):
     # return sorted_words
 
 
+def test_histogram(selected_words_list):
+    hgram = []                           # create a new list called hgram
+    for word in words:                   # for each word in the list of words
+        index = find(word, hgram)        # check if word is in hgram already
+        if index is None:                # if word is not in histogram
+            hgram.append((word, 1))      # add a new word-count pair to hgram
+        else:                            # if word is already in hgram
+            count = hgram[index][1]      # find its current count
+            new_pair = (word, count + 1) # make a new word-count pair
+            hgram[index] = new_pair      # replace word-count pair
+    return hgram                         # return the hgram
+
+
+def find(item, hgram):
+    for index, pair in enumerate(hgram):
+        if pair[0] == item:
+            return index
+    return None
+
+
 def cumulative_distribution(histogram):
     distribution_list = []
     distribution_range = 0
@@ -30,23 +50,32 @@ def cumulative_distribution(histogram):
     return distribution_list
 
 
+def list(length):
+    dict_words = '/usr/share/dict/words'
+    words_str  = open(dict_words, 'r').read()
+    all_words  = words_str.split("\n")
+    return all_words[0:length]
+
+
 def sample_from_sum(distribution_list):
     token_tuple = distribution_list[-1]
     # print(token_tuple)
     tokens = token_tuple[-1]
     # print(tokens)
+    index = random.randint(0, tokens - 1)
     for word, upper_limit in distribution_list:
-        index = random.randint(0, tokens - 1)
         if index < upper_limit:
             return word
 
 
-def randomness_check(word):
-    simulated_words = []
-    while word not in simulated_words:
-        simulated_words.append(word)
-        sample_from_sum()
-    return simulated_words
+def list(length):
+    dict_words = '/usr/share/dict/words'
+    words_str  = open(dict_words, 'r').read()
+    all_words  = words_str.split("\n")
+    return all_words[0:length]
+
+
+# def test_probability():
 
 
 def txt_to_list(file_path):
@@ -60,11 +89,12 @@ def txt_to_list(file_path):
 
 def main():
     word_list = txt_to_list(file_path)
-    x = histogram(word_list)
-    y = cumulative_distribution(x)
-    w = sample_from_sum(y)
-    return randomness_check(w)
-
+    histogram_dict = histogram(word_list)
+    distribution_list = cumulative_distribution(histogram_dict)
+    word = sample_from_sum(distribution_list)
+    # words_in_list = list(10)
+    # print(words_in_list)
+    return word
 
 if __name__ == '__main__':
-    print(main())
+    main()
