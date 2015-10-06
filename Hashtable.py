0a6b5ab5142039bcd6672b5a7647676cfdc8fa4e
cmaher92 <connor.maher@students.makeschool.com>
2015-10-06 14:31:44 -0700

    Added comments to hashtable class.

--- Hashtable.py
+++ Hashtable.py
@@ -1,30 +1,30 @@
 class Hashtable:
 
     def __init__(self):
-        self.hashtable = []  # creates the list type for the table
+        self.hashtable = []                                 # creates the list type for the table
         while len(self.hashtable) < 10:
-            self.hashtable.append(LinkedList())  # adds linked lists to hashtable
+            self.hashtable.append(LinkedList())             # adds linked lists to hashtable
         self.size = 0
         self.keys = []
 
     def set(self, key, data):
-        index_key = hash(key) % len(self.hashtable)  # bucket index
-        self.hashtable[index_key].insert([key, data])
-        self.size += 1
-        self.keys.append(key)
+        index_key = hash(key) % len(self.hashtable)         # bucket index, uses division method
+        self.hashtable[index_key].insert([key, data])       # inserts key and data to the proper bucket
+        self.size += 1                                      # increments the size of the Hashtable
+        self.keys.append(key)                               # appends the key to the keys list
 
     def get(self, key):
-        index_key = hash(key) % len(self.hashtable)
-        return self.hashtable[index_key].return_data(key)
+        index_key = hash(key) % len(self.hashtable)         # division method to determine bucket
+        return self.hashtable[index_key].return_data(key)   # returns the key from the selected bucket
 
     def update(self, key, data):
-        index_key = hash(key) % len(sef.hashtable)
-        self.hashtable[index_key].find(key).data[1] = data
+        index_key = hash(key) % len(sef.hashtable)          # determines bucket based on the given key
+        self.hashtable[index_key].find(key).data[1] = data  # finds the data based on the provided key in the selected bucket
 
-    def get_keys(self):
+    def get_keys(self):                                     # returns all of the keys
         return self.keys
 
-    def get_values(self):
+    def get_values(self):                                   # returns values
         values = []
         for key in self.keys:
             values.append(self.get(key))
